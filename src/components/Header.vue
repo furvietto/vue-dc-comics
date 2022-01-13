c34df72dbaf1ab94fb7a87dcbc66ace30c7abb83
furvietto <fulvio-94@hotmail.it>
2022-01-13 16:19:18 +0100

    add header e main

---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+.<template>
+  <header>
+      <div class="container">
+          <img src="../assets/img/dc-logo.png" alt="">
+          <ul>
+              <li>CHARACTERS</li>
+              <li class="active">COMICS</li>
+              <li>MOVIES</li>
+              <li>TV</li>
+              <li>GAMES</li>
+              <li>COLLECTIBLES</li>
+              <li>VIDEOS</li>
+              <li>FANS</li>
+              <li>NEWS</li>
+              <li>SHOP</li>
+          </ul>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+    height: 10vh;
+    font-size: 0.7em;
+    padding: 10px 0;
+    display: flex;
+    justify-content: space-between;
+        & > ul {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+                li {
+                  padding:0 10px;
+                  height: 100%;
+                  display: flex;
+                  align-items: center;
+                  border-bottom: 2px solid transparent;  
+                  cursor: pointer;
+                  &:hover,
+                  &:active,
+                  &.active{
+                      color: blue;
+                      border-bottom: 2px solid blue;
+                  }
+                }
+        }
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,90 @@
+.<template>
+    <main>
+        <div class="content">
+            <div class="container">
+                <h1>--> Content goes here </h1>
+            </div>
+        </div>
+        <div class="navPage">
+            <div class="container">
+                <ul>
+                    <li>
+                        <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                        DIGITAL COMICS
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                        DC MERCHANDISE
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                        SUBSCRIPTIONS
+                    </li>
+                    <li>
+                        <img id="small" src="../assets/img/buy-comics-shop-locator.png" alt="">
+                        COMIC SHOP LOCATOR
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                        DC POWER VISA
+                    </li>
+                    
+                </ul>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "Main",
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+main {
+    .content {
+    height: 10vh;
+    background-color: black;
+    color: white;
+    display: flex;
+    align-items: center;
+    }
+    .navPage {
+        height: 20vh;
+        background-color: blue;
+        color: white;
+        .container {
+            height: 100%;
+                ul {
+                display: flex;
+                justify-content: space-around;
+                align-items: center;
+                height: 100%;
+                font-size: 0.7em;
+                        li{
+                            img {
+                            margin-right: 10px;
+                            width: 45px;
+                            vertical-align: middle;
+                            } 
+
+                            #small{
+                                width: 35px;
+                            }
+                        } 
+                    
+                
+        }
+        }
+        
+    }
+}
+
+
+</style>
